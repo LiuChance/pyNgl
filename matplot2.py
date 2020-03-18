04f085cbb7a1462f03896e3f17f9132b8a72c39f
liuchang <408634553@qq.com>
2020-03-19 00:03:28 +0800

    Try some matplot demo

--- matplot2.py
+++ matplot2.py
@@ -1,5 +1,17 @@
+#引用部分
 import numpy as np
+import numpy as pd
 import matplotlib.pyplot as plt
 
+plt.rcParams['font.sans-serif']=['SimHei']
+plt.rcParams['axes.unicode_minus']=False
+
 #数据准备
-#x=np.arange(
+#pandas
+#pd.dataframe:处理，操作结构化数据的利器
+
+df = pd.read_csv('xwdata.csv')
+df['资料时间']=pd.to_datetime(df['资料时间'])
+df['站名']=pd.
+t = np.array(df['资料时间'])
+
